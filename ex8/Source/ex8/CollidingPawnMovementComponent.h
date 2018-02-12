bbfe35bd9921f36b82da2c981078cb82f2d584d2
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2018-02-12 11:35:22 -0700

    starting ex8

---
+++ ex8/Source/ex8/CollidingPawnMovementComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "CollidingPawnMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EX8_API UCollidingPawnMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ ex8/Source/ex8/ex8GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ex8GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EX8_API Aex8GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
