e51b2867851a1417dca7115cad4bda0faf69c162
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-03-05 13:17:10 -0700

    Fixed ex2

--- ex2/Source/ex2/FloatingActor.cpp
+++ ex2/Source/ex2/FloatingActor.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FloatingActor.h"
+#include "Engine/Engine.h"
 
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White,text)
 
 // Sets default values
 AFloatingActor::AFloatingActor()
